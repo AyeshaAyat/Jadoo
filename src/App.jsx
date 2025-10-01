0441a2accf610e2a1115ddf8f30d51e67e38ff8d
AyeshaAyat <ayeshaayat184@gmail.com>
2025-10-01 14:03:54 +0600

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Desitnations from './components/pages/Desitnations';
 import Hotels from './components/pages/Hotels';
 import Flights from './components/pages/Flights';
 import Bookings from './components/pages/Bookings';
+import Login from './components/Login';
+import Signup from './Signup';
 function App() {
 
 
@@ -18,6 +20,8 @@ function App() {
         <Route path='hotels/' element={<Hotels/>}/>
         <Route path='flights/' element={<Flights/>}/>
         <Route path='bookings/' element={<Bookings/>}/>
+        <Route path='login/' element={<Login/>}/>
+        <Route path='signup/' element={<Signup/>}/>
     </Route>
    </Routes>
    </>
--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -6,83 +6,87 @@ import Flex from "../flex";
 import { Link } from "react-router-dom";
 import { FaAngleDown } from "react-icons/fa";
 import bannerLetter from "../../assets/Desc.png";
+import banner from "../../assets/Hero Content.png";
 import bannergirl from "../../assets/bannergirl.png";
+
 const Navbar = () => {
-  return (
-    <>
-        <div className="bg-[url('/src/assets/Decore.png')]  w-[1500px] m-auto  bg-right-top  bg-no-repeat p-5">
-      <Container>
-        {/* ---------------------hearderPart----------- */}
-        <div className="py-10 ">
+    return (
+        <>
+            <div className="bg-[url('/src/assets/Decore.png')]  w-[1450px] m-auto  bg-right-top  bg-no-repeat p-5">
+                <Container>
+                    {/* ---------------------hearderPart----------- */}
+                    <div className="pt-10 ">
 
-          <Flex className="justify-between">
-            <Link to="/">
-              <div className="">
-                <Image
-                  imgSrc={logo}
-                  imgAlt={logo}
-                  className="h-[33px] w-[114px]"
-                />
-              </div>
-            </Link>
-            <div className="flex gap-x-4 px-10">
-              <ul className="gap-x-20 px-5  flex justify-between">
-            
-                  <Link to="desitination/">
-                    <li className="text-[22px] font-sans text-[#212832]">
-                      Desitnations
-                    </li>
-                  </Link>
-                  <Link to="hotels/">
-                    <li className="text-[22px] font-sans text-[#212832]">
-                      Hotels
-                    </li>
-                  </Link>
-                  <Link to="flights/">
-                    <li className="text-[22px] font-sans text-[#212832]">
-                      Flights
-                    </li>
-                  </Link>
-                  <Link to="bookings/">
-                    <li className="text-[22px] font-sans text-[#212832]">
-                      Bookings
-                    </li>
-                  </Link>
-               
-              </ul>
-              <button className="h-[50px] w-[130px] pb-10 rounded-[10px] text-[22px] text-[#212832] cursor-pointer ">
-                Login
-              </button>
-              <button className="h-[50px] w-[130px] pb-2 rounded-[10px] text-[22px] text-[#212832] cursor-pointer border-1 border:mb-2">
-                Sign up
-              </button>
-              <div className=" flex px-7 gap-x-3 justify-between py-2 pb-10  text-[22px] text-[#212832] cursor-pointer ">
-              <>
-              <p>EN</p>
-                
-                <FaAngleDown className="mt-2 text-[#212832] opacity-70"/>
+                        <Flex className="justify-between">
+                            <Link to="/">
+                                <div className="">
+                                    <Image
+                                        imgSrc={logo}
+                                        imgAlt={logo}
+                                        className="h-[33px] w-[114px]"
+                                    />
+                                </div>
+                            </Link>
+                            <div className="flex gap-x-4 px-10">
+                                <ul className="gap-x-18 px-5  flex justify-between">
 
-              </>
-              </div>
+                                    <Link to="desitination/">
+                                        <li className="text-[22px] font-sans text-[#212832]">
+                                            Desitnations
+                                        </li>
+                                    </Link>
+                                    <Link to="hotels/">
+                                        <li className="text-[22px] font-sans text-[#212832]">
+                                            Hotels
+                                        </li>
+                                    </Link>
+                                    <Link to="flights/">
+                                        <li className="text-[22px] font-sans text-[#212832]">
+                                            Flights
+                                        </li>
+                                    </Link>
+                                    <Link to="bookings/">
+                                        <li className="text-[22px] font-sans text-[#212832]">
+                                            Bookings
+                                        </li>
+                                    </Link>
+                                    <Link to="login/">
+                                        <li className=" ml-2 text-[22px] font-sans text-[#212832]">
+
+                                            Login
+                                        </li>
+                                    </Link>
+                                    <Link to=" signup/">
+                                        <li className=" py-1 mr-2  px-4 text-center  border-[#212832c6] rounded-[6px] text-[22px] text-[#212832] cursor-pointer border-1">
+                                            Sign up
+                                        </li>
+                                    </Link>
+
+                                </ul>
+
+                                <div className=" flex pr-7 gap-x-3 justify-between py-2 pb-10  text-[22px] text-[#212832] cursor-pointer ">
+                                    <>
+                                        <p>EN</p>
+
+                                        <FaAngleDown className="mt-2 text-[#212832] opacity-70" />
+
+                                    </>
+                                </div>
+                            </div>
+                        </Flex>
+                        {/* ---------------------hearderPart----------- */}
+
+                    </div>
+                    {/* ----------------------bannerPart-------------- */}
+                    <div className="m-auto">
+                        <Image imgSrc={banner} imgAlt={banner} className='mb-10 px-8  w-[1400px] h-[770px] bg-cover bg-center bg-no-repeat' />
+                    </div>
+
+                    {/* ----------------------bannerPart-------------- */}
+                </Container>
             </div>
-          </Flex>
-                  {/* ---------------------hearderPart----------- */}
-
-        </div>
-        {/* ----------------------bannerPart-------------- */}
-          <Container>
-            <div className=" px-10">
-              <Flex>
-                <Image imgSrc={bannerLetter} imgAlt={bannerLetter} className='mt-18 ml-5 h-[535px] w-[630px]' />
-                <Image imgSrc={bannergirl} imgAlt={bannergirl} className='h-[764px] w-[783px]' />
-              </Flex>
-            </div>
-          </Container>
-      </Container>
-        {/* ----------------------bannerPart-------------- */}
-        </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default Navbar;
