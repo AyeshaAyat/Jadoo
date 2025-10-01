99c29aa0704168ea1cedf75ebb5cc350342edc0f
AyeshaAyat <ayeshaayat184@gmail.com>
2025-10-01 17:08:24 +0600

    cut

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,8 +1,4 @@
-import Container from "../Container"
-import Flex from "../flex"
-import Image from "../Image"
-import bannerLetter from '../../assets/Desc.png'
-import bannergirl from '../../assets/bannergirl.png'
+
 const Home = () => {
   return (
     <>
