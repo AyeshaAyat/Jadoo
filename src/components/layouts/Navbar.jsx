f5258842e45b76724d88ab5316851695847d555a
AyeshaAyat <ayeshaayat184@gmail.com>
2025-10-05 21:18:20 +0600

    Default Screen fix

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -7,45 +7,47 @@ import { Link } from "react-router-dom";
 import { FaAngleDown } from "react-icons/fa";
 
 import banner from "../../assets/Hero Content.png";
+import bannergirl from "../../assets/bannergirl.png";
+import bannerTxt from "../../assets/Desc.png";
 
 const Navbar = () => {
   return (
     <>
       <Container>
-        <div className="bg-[url('/src/assets/Decore.png')] m-auto  bg-right-top bg-contain  bg-no-repeat ">
+        <div className="bg-[url('/src/assets/Decore.png')]   bg-right-top bg-fit  bg-no-repeat ">
           {/* ---------------------hearderPart----------- */}
-          <div className=" main py-7 flex gap-50 ">
-            <div className="pl-12">
+          <div className=" main pt-6   flex gap-40 ">
+            <div className="pl-25 ">
               <Image imgSrc={logo} imgAlt={logo} />
             </div>
             <div className="flex mt-2">
-              <ul className="gap-x-10 flex ">
-                <li className="text-[14px] font-[450] font-sans text-[#212832]">
+              <ul className="gap-x-12 flex ">
+                <li className="text-[14px] font-medium font-sans text-[#212832]">
                   Desitnations
                 </li>
 
-                <li className="text-[14px] font-[450] font-sans text-[#212832]">
+                <li className="text-[14px] font-medium font-sans text-[#212832]">
                   Hotels
                 </li>
 
-                <li className="text-[14px] font-[450] font-sans text-[#212832]">
+                <li className="text-[14px] font-medium font-sans text-[#212832]">
                   Flights
                 </li>
 
-                <li className="text-[14px] font-[450] font-sans text-[#212832]">
+                <li className="text-[14px] font-medium font-sans text-[#212832]">
                   Bookings
                 </li>
 
-                <li className="text-[14px] font-[450] font-sans text-[#212832]">
+                <li className="text-[14px] pl-2 font-medium font-sans text-[#212832]">
                   Login
                 </li>
 
-                <li className="  border-1 py-0.5  px-2.5 text-center font-[500] rounded-[2px] text-[12px] text-[#212832] cursor-pointer ">
+                <li className="  border-1 py-0.5  px-2.5 text-center font-medium rounded-[2px] text-[14px] text-[#212832] cursor-pointer ">
                   Sign up
                 </li>
               </ul>
 
-              <li className="ml-5 flex font-[450]  text-[14px] text-[#212832] cursor-pointer ">
+              <li className="ml-10 flex font-medium  text-[14px] text-[#212832] cursor-pointer ">
                 <>
                   EN
 
@@ -57,13 +59,17 @@ const Navbar = () => {
             {/* ---------------------hearderPart----------- */}
           </div>
           {/* ----------------------bannerPart-------------- */}
-          <div className="m-auto pl-6 flex">
-            <Image
-              imgSrc={banner}
-              imgAlt={banner}
-              className="h-[650px]  bg-cover bg-center bg-no-repeat"
-            />
+        <div className="BannerMain ">
+          <div className="Girl flex relative">
+
+              <Image imgSrc={banner}  className="ml-20  mt-3 h-[560px] w-[970px]"  imgAlt={banner} />
+              {/* <Image imgSrc={bannerTxt}  className="ml-20 mt-12 h-[315px] w-[392px]"  imgAlt={bannerTxt} />
+              <Image imgSrc={bannergirl} className="absolute left-105 h-[420px] w-[440px] " imgAlt={bannergirl} />
+            <div className="text">
+
+            </div> */}
           </div>
+        </div>
 
           {/* ----------------------bannerPart-------------- */}
         </div>
